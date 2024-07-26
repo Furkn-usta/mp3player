5067e575715bde4a6e646e96a1f046d7379b68d6
Furkan Usta <92405866+Furkn-usta@users.noreply.github.com>
2024-07-26 14:58:54 +0300

    Add files via upload

---
+++ SoundsDJ/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.volumeTrackBar = new System.Windows.Forms.TrackBar();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.labelFrequencies = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openWaveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.volumeTrackBar)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(45, 39);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(269, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Open Mp3 File";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(45, 68);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(269, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Pause/Play WAV";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(45, 141);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(269, 45);
+            this.trackBar1.TabIndex = 2;
+            this.trackBar1.TickStyle = System.Windows.Forms.TickStyle.TopLeft;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // volumeTrackBar
+            // 
+            this.volumeTrackBar.BackColor = System.Drawing.SystemColors.Control;
+            this.volumeTrackBar.Location = new System.Drawing.Point(333, 38);
+            this.volumeTrackBar.Maximum = 100;
+            this.volumeTrackBar.Name = "volumeTrackBar";
+            this.volumeTrackBar.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.volumeTrackBar.Size = new System.Drawing.Size(45, 104);
+            this.volumeTrackBar.TabIndex = 3;
+            this.volumeTrackBar.TickStyle = System.Windows.Forms.TickStyle.Both;
+            this.volumeTrackBar.Value = 50;
+            this.volumeTrackBar.Scroll += new System.EventHandler(this.volumeTrackBar_Scroll);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(31, 192);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 147);
+            this.listBox1.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 348);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(10, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "-";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(45, 97);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(269, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Play Music";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // labelFrequencies
+            // 
+            this.labelFrequencies.AutoSize = true;
+            this.labelFrequencies.Location = new System.Drawing.Point(247, 193);
+            this.labelFrequencies.Name = "labelFrequencies";
+            this.labelFrequencies.Size = new System.Drawing.Size(28, 13);
+            this.labelFrequencies.TabIndex = 7;
+            this.labelFrequencies.Text = "Freq";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(409, 24);
+            this.menuStrip1.TabIndex = 8;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openWaveToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openWaveToolStripMenuItem
+            // 
+            this.openWaveToolStripMenuItem.Name = "openWaveToolStripMenuItem";
+            this.openWaveToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.openWaveToolStripMenuItem.Text = "Open Wave";
+            this.openWaveToolStripMenuItem.Click += new System.EventHandler(this.openWaveToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(409, 370);
+            this.Controls.Add(this.labelFrequencies);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.volumeTrackBar);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "First Tutorial";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.volumeTrackBar)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TrackBar volumeTrackBar;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label labelFrequencies;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openWaveToolStripMenuItem;
+    }
+}
+
---
+++ SoundsDJ/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,276 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using NAudio.Wave;
+using NAudio.Dsp;
+using NAudio.Wave.SampleProviders;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private Mp3FileReader mp3Reader = null;
+        private WaveOutEvent output = null;
+        private VolumeSampleProvider volumeProvider = null;
+        private List<string> musicFiles = new List<string>();
+        private ISampleProvider sampleProvider;
+        private Complex[] fftBuffer;
+        private int fftPos;
+        private int fftLength = 1024; // 1024-point FFT
+        private int m = (int)Math.Log(1024, 2); // Log2(fftLength)
+        private bool fftCalculated = false;
+        private Timer fftTimer;
+        private int currentTrackIndex = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeFFT();
+
+            // MessageBox.Show("Form1 Loaded"); // Debugging için ekledik
+        }
+
+        private float playbackSpeed = 1.0f; // Varsayılan hız: 1.0f (normal hız)
+
+        private void InitializeFFT()
+        {
+            fftBuffer = new Complex[fftLength];
+            fftTimer = new Timer();
+            fftTimer.Interval = 50; // 50ms interval for FFT analysis
+            fftTimer.Tick += FftTimer_Tick;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "MP3 Files (*.mp3)|*.mp3";
+            ofd.Multiselect = true; // Çoklu seçim için izin ver
+            if (ofd.ShowDialog() != DialogResult.OK) return;
+
+            foreach (var file in ofd.FileNames)
+            {
+                musicFiles.Add(file);
+                listBox1.Items.Add(System.IO.Path.GetFileName(file));
+            }
+
+            label1.Text = "Total Music Files: " + musicFiles.Count.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (output != null)
+            {
+                if (output.PlaybackState == PlaybackState.Playing) output.Pause();
+                else if (output.PlaybackState == PlaybackState.Paused) output.Play();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex != -1)
+            {
+                currentTrackIndex = listBox1.SelectedIndex;
+                PlayTrack(currentTrackIndex);
+            }
+        }
+        private void PlayTrack(int trackIndex)
+        {
+            if (trackIndex >= 0 && trackIndex < musicFiles.Count)
+            {
+                DisposeWave();
+
+                // Müzik dosyasını okuyan bir ISampleProvider oluşturun
+                mp3Reader = new Mp3FileReader(musicFiles[trackIndex]);
+                sampleProvider = mp3Reader.ToSampleProvider();
+
+                // Ses kontrolü için VolumeSampleProvider kullanın
+                volumeProvider = new VolumeSampleProvider(sampleProvider)
+                {
+                    Volume = volumeTrackBar.Value / 100.0f
+                };
+
+                // Hız değiştirme işlemi için OffsetSampleProvider oluşturun
+                OffsetSampleProvider offsetSampleProvider = new OffsetSampleProvider(volumeProvider);
+                offsetSampleProvider.SkipOver = TimeSpan.FromSeconds(0); // Başlangıçta atlanacak süre
+                offsetSampleProvider.Take = TimeSpan.FromSeconds(mp3Reader.TotalTime.TotalSeconds); // Müziğin tüm süresi
+
+                // WaveOutEvent nesnesi oluşturun ve müziği çalın
+                output = new WaveOutEvent();
+                output.PlaybackStopped += Output_PlaybackStopped; // Olayı buraya bağlayın
+                output.Init(offsetSampleProvider);
+                output.Play();
+
+                trackBar1.Maximum = (int)mp3Reader.TotalTime.TotalSeconds;
+                timer1.Start();
+                fftTimer.Start();
+
+                button2.Enabled = true;
+
+                // Playback durumunu kontrol edelim
+                //  MessageBox.Show("Playback state: " + output.PlaybackState); // Debugging için ekledik
+            }
+        }
+
+        private void Output_PlaybackStopped(object sender, StoppedEventArgs e)
+        {
+            //  MessageBox.Show("Playback stopped!"); // Debugging için ekledik
+            if (InvokeRequired)
+            {
+                Invoke(new Action<object, StoppedEventArgs>(Output_PlaybackStopped), sender, e);
+                return;
+            }
+
+            // Move to the next track in the list
+            currentTrackIndex++;
+            if (currentTrackIndex < musicFiles.Count)
+            {
+                listBox1.SelectedIndex = currentTrackIndex;
+                PlayTrack(currentTrackIndex);
+            }
+            else
+            {
+                // Stop timers if all tracks are played
+                timer1.Stop();
+                fftTimer.Stop();
+                currentTrackIndex = 0; // Reset to the first track
+                StopPlayback();
+            }
+        }
+
+        private void DisposeWave()
+        {
+            if (output != null)
+            {
+                output.PlaybackStopped -= Output_PlaybackStopped; // Olayı kaldırın
+                if (output.PlaybackState == PlaybackState.Playing) output.Stop();
+                output.Dispose();
+                output = null;
+            }
+            if (mp3Reader != null)
+            {
+                mp3Reader.Dispose();
+                mp3Reader = null;
+            }
+            fftTimer.Stop();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            DisposeWave();
+        }
+
+        private void StopPlayback()
+        {
+            // Çalma işlemini durdur
+            DisposeWave();
+            // Diğer gerekli işlemler...
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            if (mp3Reader != null)
+            {
+                mp3Reader.CurrentTime = TimeSpan.FromSeconds(trackBar1.Value);
+            }
+            // trackBar değeri 10 olduğunda bir sonraki şarkıyı çal
+            if (trackBar1.Value == 10)
+            {
+                // Şu an çalan şarkının index'ini bir artırarak bir sonraki şarkıyı çal
+                currentTrackIndex++;
+                if (currentTrackIndex < musicFiles.Count)
+                {
+                    listBox1.SelectedIndex = currentTrackIndex;
+                    PlayTrack(currentTrackIndex);
+                }
+                else
+                {
+                    // Eğer sonraki şarkı yoksa, işlemi durdur
+                    StopPlayback();
+                }
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (mp3Reader != null)
+            {
+                trackBar1.Value = (int)mp3Reader.CurrentTime.TotalSeconds;
+            }
+        }
+
+        private void volumeTrackBar_Scroll(object sender, EventArgs e)
+        {
+            if (volumeProvider != null)
+            {
+                volumeProvider.Volume = volumeTrackBar.Value / 100.0f;
+            }
+        }
+
+        private void FftTimer_Tick(object sender, EventArgs e)
+        {
+            /*
+            if (sampleProvider != null)
+            {
+                float[] buffer = new float[fftLength];
+                int samplesRead = sampleProvider.Read(buffer, 0, fftLength);
+
+                if (samplesRead > 0)
+                {
+                    // Hızı kullanarak okunan örnek sayısını ayarlayın
+                    int adjustedSampleCount = (int)(samplesRead / playbackSpeed);
+
+                    for (int i = 0; i < adjustedSampleCount; i++)
+                    {
+                        fftBuffer[fftPos].X = (float)(buffer[i] * FastFourierTransform.HammingWindow(fftPos, fftLength));
+                        fftBuffer[fftPos].Y = 0;
+                        fftPos++;
+                        if (fftPos >= fftLength)
+                        {
+                            fftPos = 0;
+                            fftCalculated = true;
+                        }
+                    }
+
+                    if (fftCalculated)
+                    {
+                        fftCalculated = false;
+                        FastFourierTransform.FFT(true, m, fftBuffer);
+                        float maxFreq = 0;
+                        float maxAmplitude = float.MinValue;
+                        for (int i = 0; i < fftBuffer.Length / 2; i++)
+                        {
+                            float amplitude = fftBuffer[i].X * fftBuffer[i].X + fftBuffer[i].Y * fftBuffer[i].Y;
+                            if (amplitude > maxAmplitude)
+                            {
+                                maxAmplitude = amplitude;
+                                maxFreq = i;
+                            }
+                        }
+                        float freq = maxFreq * sampleProvider.WaveFormat.SampleRate / fftBuffer.Length;
+                        labelFrequencies.Invoke((MethodInvoker)delegate
+                        {
+                            labelFrequencies.Text = $"Frequency: {freq} Hz";
+                        });
+                    }
+                }
+            }
+            */
+        }
+
+        private void openWaveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "MP3 Files (*.mp3)|*.mp3";
+            ofd.Multiselect = true; // Çoklu seçim için izin ver
+            if (ofd.ShowDialog() != DialogResult.OK) return;
+
+            foreach (var file in ofd.FileNames)
+            {
+                musicFiles.Add(file);
+                listBox1.Items.Add(System.IO.Path.GetFileName(file));
+            }
+
+            label1.Text = "Total Music Files: " + musicFiles.Count.ToString();
+        }
+    }
+}
